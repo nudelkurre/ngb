23784f5428743a0b872945ba42ca0b865dae9dab
Emil Wendin <nudelkurre@protonmail.com>
2025-02-15 16:01:56 +0100

    Added nix devShell with all needed packages and configuration to run project

--- flake.nix
+++ flake.nix
@@ -20,6 +20,27 @@
           default = pybar;
           pybar = flake-utils.lib.mkApp { drv = self.packages.${system}.pybar; };
         };
+        devShells = rec {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              gobject-introspection
+              gtk4
+              gtk4-layer-shell
+              hatch
+              makeWrapper
+              python3Packages.psutil
+              python3Packages.pygobject3
+              python3Packages.screeninfo
+              wrapGAppsHook
+            ];
+            shellHook = ''
+              export PS1='\n\[\033[1;32m\][nix-shell:\w]\[\033[1;31m\](Dev)\[\033[1;32m\]\$\[\033[0m\]'
+            '';
+            env = {
+              "LD_LIBRARY_PATH" = "$out/lib:${pkgs.gtk4-layer-shell}/lib:$LD_LIBRARY_PATH";
+            };
+          };
+        };
       }
     );
 }
