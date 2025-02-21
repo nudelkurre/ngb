fdf7c4fd9649396c691250344be5551f651dfae4
Emil Wendin <nudelkurre@protonmail.com>
2025-02-21 23:51:27 +0100

    Added default timeformat to Clock()

--- src/ngb/widgets/clock.py
+++ src/ngb/widgets/clock.py
@@ -10,7 +10,7 @@ class Clock(WidgetBox):
     text_label = Gtk.Label()
     icon_label = Gtk.Label()
 
-    def __init__(self, timeformat):
+    def __init__(self, timeformat="%Y-%m-%d %H:%M:%S"):
         WidgetBox.__init__(self, icon="", spacing=4)
         self.timeformat = timeformat
 
