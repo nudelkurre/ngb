f0215bd9efd320a83cce59662de416efdb089de8
Emil Wendin <nudelkurre@protonmail.com>
2025-02-26 04:36:31 +0100

    Added to change timeformat when hover over clock

--- src/ngb/widgets/clock.py
+++ src/ngb/widgets/clock.py
@@ -6,11 +6,21 @@ from ngb.modules import WidgetBox
 
 class Clock(WidgetBox):
     timeformat = ""
-    def __init__(self, timeformat="%Y-%m-%d %H:%M:%S"):
-        super().__init__(icon="", spacing=4)
-        self.timeformat = timeformat
+    def __init__(self, timeformat_normal="%T", timeformat_hover="%Y-%m-%d %H:%M:%S"):
+        super().__init__(icon="", spacing=4, timer=1000)
+        self.timeformat = timeformat_normal
+        self.timeformat_normal = timeformat_normal
+        self.timeformat_hover = timeformat_hover
 
     def set_text(self):
         datetimenow = datetime.now().strftime(self.timeformat)
         self.text_label.set_text(datetimenow)
         return True
+
+    def on_hover_enter(self, controller, x, y):
+        self.timeformat = self.timeformat_hover
+        self.set_text()
+
+    def on_hover_leave(self, controller):
+        self.timeformat = self.timeformat_normal
+        self.set_text()
