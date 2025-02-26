87a452650cad893ba7153b72a9d5a31a6c9b9dfe
Emil Wendin <nudelkurre@protonmail.com>
2025-02-26 04:35:35 +0100

    Added support for events for click, scroll and hover for widgetbox

--- src/ngb/modules/widgetbox.py
+++ src/ngb/modules/widgetbox.py
@@ -1,5 +1,6 @@
 from gi.repository import Gtk
 from gi.repository import GLib
+from gi.repository import Gdk
 
 class WidgetBox(Gtk.Box):
     def __init__(self, icon="", icon_size=20, text="", timer=1000, spacing=10):
@@ -10,11 +11,33 @@ class WidgetBox(Gtk.Box):
         self.icon_size = icon_size
         self.icon_label = Gtk.Label()
         self.text_label = Gtk.Label()
+        self.scroll_controller = Gtk.EventControllerScroll.new(Gtk.EventControllerScrollFlags.VERTICAL)
+        self.scroll_controller.connect("scroll", self.on_scroll)
+        self.add_controller(self.scroll_controller)
+        self.hover_controller = Gtk.EventControllerMotion.new()
+        self.hover_controller.connect("enter", self.on_hover_enter)
+        self.hover_controller.connect("leave", self.on_hover_leave)
+        self.add_controller(self.hover_controller)
+        self.click_controller = Gtk.GestureClick.new()
+        self.click_controller.connect("pressed", self.on_click)
+        self.add_controller(self.click_controller)
         self.append(self.icon_label)
         self.append(self.text_label)
         self.set_icon()
         self.set_text()
         self.update_label()
+
+    def on_scroll(self, controller, x, y):
+        pass
+
+    def on_hover_enter(self, controller, x, y):
+        pass
+
+    def on_hover_leave(self, controller):
+        pass
+
+    def on_click(self, controller, n_press, x, y):
+        pass
 
     def set_icon(self):
         self.icon_label.set_markup(f"<span size=\"{self.icon_size * 1000}\">{self.icon}</span>")
