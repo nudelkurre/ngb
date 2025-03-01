41268c492355baa3293b713574a78e02c93c1107
Emil Wendin <nudelkurre@protonmail.com>
2025-03-01 03:27:34 +0100

    Added change default sink to right click on volume

--- src/ngb/widgets/volume.py
+++ src/ngb/widgets/volume.py
@@ -1,6 +1,7 @@
 from gi.repository import Gtk
 from gi.repository import GLib
 import subprocess
+import re
 
 from ngb.modules import WidgetBox
 
@@ -32,5 +33,29 @@ class Volume(WidgetBox):
             volume_cmd = f"{volume_cmd} 5%-".split()
             subprocess.run(volume_cmd)
 
-    def on_click(self, controller, n_press, x, y):
+    def on_click(self, sequence, user_data):
         subprocess.run("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle".split())
+
+    def on_right_click(self, sequence, user_data):
+        wpctl = subprocess.run("wpctl status".split(), capture_output=True, text=True).stdout#.split("\n\n")
+
+        sinks = re.search(r"Audio\n([\W\w]*)Video", wpctl).group(1)
+        sinks = re.search(r"Sinks:\n([\W\w]*)Sources", sinks).group(1).split("\n")[:-2]
+        sink_list = []
+
+        for i in sinks:
+            match = re.search(r"\s*(\*?)\s*(\d+)\.\s*([\w\s\d]+)\s*\[vol:\s*(\d+\.\d+)\]", i)
+            if(match):
+                sink = {"id": match.group(2),
+                    "name": match.group(3).lstrip().rstrip(),
+                    "volume": int(float(match.group(4)) * 100),
+                    "default": True if match.group(1) == "*" else False
+                }
+                sink_list.append(sink)
+
+        default = 0
+        for index, sink in enumerate(sink_list):
+            if(sink["default"]):
+                default = index
+        
+        subprocess.run(f"wpctl set-default {sink_list[(default + 1) % len(sink_list)]['id']}".split())
