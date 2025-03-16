45afcbc6177ec2b5a92412218f4c128e029290a8
Emil Wendin <nudelkurre@protonmail.com>
2025-03-16 23:24:30 +0100

    Added dropdown for disk

--- src/ngb/widgets/disk.py
+++ src/ngb/widgets/disk.py
@@ -11,17 +11,33 @@ class Disk(WidgetBox):
         self.icon = kwargs.get("icon", "")
         self.timer = kwargs.get("timer", 10)
         self.icon_size = kwargs.get("icon_size", 20)
+        self.mountpoint_label = Gtk.Label()
+        self.storage_label = Gtk.Label()
+        self.used_bar = Gtk.ProgressBar()
         super().__init__(timer=self.timer, icon=self.icon, icon_size=self.icon_size)
+        self.populate_dropdown()
 
     def set_text(self):
         self.get_disk_usage()
         return True
 
+    def populate_dropdown(self):
+        self.dropdown.add(self.mountpoint_label)
+        self.dropdown.add(self.used_bar)
+        self.dropdown.add(self.storage_label)
+
     def get_disk_usage(self):
         disk_data = psutil.disk_usage(self.mountpoint)
         percent = disk_data.percent
         self.text_label.set_label(f"{percent}%")
+        self.mountpoint_label.set_label(f"{self.mountpoint}")
+        self.storage_label.set_label(f"{bytes2human(disk_data.used)}iB/{bytes2human(disk_data.total)}iB")
+        self.used_bar.set_fraction(disk_data.used / disk_data.total)
         return True
 
     def update_disk_usage(self):
         GLib.timeout_add(self.timer, self.get_disk_usage)
+
+    def on_click(self, user_data):
+        self.dropdown.popup()
+        return True
