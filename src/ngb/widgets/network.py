0237fe95e9705e2e3366309886b9881462505052
Emil Wendin <nudelkurre@protonmail.com>
2025-03-16 23:52:09 +0100

    Added dropdown with network information

--- src/ngb/widgets/network.py
+++ src/ngb/widgets/network.py
@@ -9,27 +9,58 @@ class Network(WidgetBox):
     interface = []
     
     def __init__(self, **kwargs):
-        self.interface = psutil.net_if_addrs()[kwargs.get("interface", "")]
+        self.interface_name = kwargs.get("interface", "")
+        self.interface = psutil.net_if_addrs()[self.interface_name]
         self.timer = kwargs.get("timer", 10)
         self.icon = kwargs.get("icon", "󰈀")
         self.icon_size = kwargs.get("icon_size", 20)
+        self.interface_label = Gtk.Label(label=f"Interface: {self.interface_name}")
+        self.ipv4_label = Gtk.Label()
+        self.ipv4_label.set_justify(Gtk.Justification.CENTER)
+        self.ipv6_label = Gtk.Label()
+        self.ipv6_label.set_justify(Gtk.Justification.CENTER)
+        self.mac_address_label = Gtk.Label()
         super().__init__(icon=self.icon, timer=self.timer, icon_size=self.icon_size)
+        self.populate_dropdown()
 
     def set_text(self):
         self.get_ipv4_addr()
+        self.get_ipv6_addrs()
+        self.get_mac_address()
         return True
 
+    def populate_dropdown(self):
+        self.dropdown.add(self.interface_label)
+        self.dropdown.add(self.mac_address_label)
+        self.dropdown.add(self.ipv4_label)
+        self.dropdown.add(Gtk.Label(label="IPv6:"))
+        self.dropdown.add(self.ipv6_label)
+
     def get_ipv4_addr(self):
-        address= ""
+        address = ""
         for addr in self.interface:
             if(addr.family == socket.AF_INET and address == ""):
                 address = addr.address
         self.text_label.set_label(address)
+        self.ipv4_label.set_label(F"IPv4: {address}")
         return True
 
     def get_ipv6_addrs(self):
-        addrs: list = []
+        addrs = []
         for addr in self.interface:
             if(addr.family == socket.AF_INET6):
                 addrs.append(addr.address)
-        return addrs
+        if(len(addrs) > 0):
+            self.ipv6_label.set_label("\n".join(addrs))
+        return True
+
+    def get_mac_address(self):
+        address = ""
+        for addr in self.interface:
+            if(addr.family == socket.AF_PACKET):
+                address = addr.address
+        self.mac_address_label.set_label(f"Mac: {address}")
+
+    def on_click(self, user_data):
+        self.dropdown.popup()
+        return True
