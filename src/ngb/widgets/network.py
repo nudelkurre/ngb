2c650d922139f04e44c9b78dbaa075165019aaf8
Emil Wendin <nudelkurre@protonmail.com>
2025-03-18 21:24:56 +0100

    Changed to hide ip labels if no address on interface

--- src/ngb/widgets/network.py
+++ src/ngb/widgets/network.py
@@ -10,11 +10,13 @@ class Network(WidgetBox):
     
     def __init__(self, **kwargs):
         self.interface_name = kwargs.get("interface", "")
-        self.interface = psutil.net_if_addrs()[self.interface_name]
+        self.interface = psutil.net_if_addrs()[self.interface_name] if self.interface_name in psutil.net_if_addrs() else []
         self.timer = kwargs.get("timer", 10)
         self.icon = kwargs.get("icon", "󰈀")
         self.icon_size = kwargs.get("icon_size", 20)
         self.interface_label = Gtk.Label(label=f"Interface: {self.interface_name}")
+        self.ipv4_header_label = Gtk.Label(label="IPv4:")
+        self.ipv6_header_label = Gtk.Label(label="IPv6:")
         self.ipv4_label = Gtk.Label()
         self.ipv4_label.set_justify(Gtk.Justification.CENTER)
         self.ipv6_label = Gtk.Label()
@@ -32,8 +34,9 @@ class Network(WidgetBox):
     def populate_dropdown(self):
         self.dropdown.add(self.interface_label)
         self.dropdown.add(self.mac_address_label)
+        self.dropdown.add(self.ipv4_header_label)
         self.dropdown.add(self.ipv4_label)
-        self.dropdown.add(Gtk.Label(label="IPv6:"))
+        self.dropdown.add(self.ipv6_header_label)
         self.dropdown.add(self.ipv6_label)
 
     def get_ipv4_addr(self):
@@ -41,8 +44,15 @@ class Network(WidgetBox):
         for addr in self.interface:
             if(addr.family == socket.AF_INET and address == ""):
                 address = addr.address
-        self.text_label.set_label(address)
-        self.ipv4_label.set_label(F"IPv4: {address}")
+        if(address != ""):
+            self.text_label.set_label(address)
+            self.ipv4_label.set_label(address)
+            self.ipv4_label.set_visible(True)
+            self.ipv4_header_label.set_visible(True)
+        else:
+            self.text_label.set_label("N/A")
+            self.ipv4_label.set_visible(False)
+            self.ipv4_header_label.set_visible(False)
         return True
 
     def get_ipv6_addrs(self):
@@ -52,6 +62,11 @@ class Network(WidgetBox):
                 addrs.append(addr.address)
         if(len(addrs) > 0):
             self.ipv6_label.set_label("\n".join(addrs))
+            self.ipv6_label.set_visible(True)
+            self.ipv6_header_label.set_visible(True)
+        else:
+            self.ipv6_label.set_visible(False)
+            self.ipv6_header_label.set_visible(False)
         return True
 
     def get_mac_address(self):
@@ -59,7 +74,10 @@ class Network(WidgetBox):
         for addr in self.interface:
             if(addr.family == socket.AF_PACKET):
                 address = addr.address
-        self.mac_address_label.set_label(f"Mac: {address}")
+        if(address != ""):
+            self.mac_address_label.set_label(f"Mac: {address}")
+        else:
+            self.mac_address_label.set_label("Mac: N/A")
 
     def on_click(self, user_data):
         self.dropdown.popup()
