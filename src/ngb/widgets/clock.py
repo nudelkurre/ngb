cabceb83d2714012851d7a0157fc0f990de59da7
Emil Wendin <nudelkurre@protonmail.com>
2025-03-20 15:22:56 +0100

    Added calendar to clock

--- src/ngb/widgets/clock.py
+++ src/ngb/widgets/clock.py
@@ -14,9 +14,13 @@ class Clock(WidgetBox):
         self.timeformat_revealer = kwargs.get("timeformat_revealer", "%A %Y-%m-%d")
         self.transition_time = kwargs.get("transition_time", 500)
         self.icon_size = kwargs.get("icon_size", 20)
+        self.show_day_names = kwargs.get("show_day_names", True)
+        self.show_heading = kwargs.get("show_heading", True)
+        self.show_week_numbers = kwargs.get("show_week_numbers", True)
         self.revealer_label = Gtk.Label()
         self.show_revealer = False
         super().__init__(icon=self.icon, spacing=self.spacing, timer=self.timer, icon_size=self.icon_size)
+        self.populate_dropdown()
         
         # Create a revealer for smoother transition when hover over
         self.revealer = Gtk.Revealer()
@@ -25,6 +29,12 @@ class Clock(WidgetBox):
         self.revealer.set_transition_duration(self.transition_time)
         self.box.insert_child_after(self.revealer, self.icon_label)
 
+    def populate_dropdown(self):
+        calendar = Gtk.Calendar()
+        calendar.set_show_day_names(self.show_day_names)
+        calendar.set_show_heading(self.show_heading)
+        calendar.set_show_week_numbers(self.show_week_numbers)
+        self.dropdown.add(calendar)
 
     def set_text(self):
         datetimenow = datetime.now().strftime(self.timeformat)
@@ -36,3 +46,6 @@ class Clock(WidgetBox):
     def on_click(self, user_data):
         self.show_revealer = not self.show_revealer
         self.revealer.set_reveal_child(self.show_revealer)
+
+    def on_right_click(self, sequence, user_data):
+        self.dropdown.popup()
