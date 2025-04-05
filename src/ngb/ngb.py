ddb29e5b6c3354b16e53a2408902633b238046ac
Emil Wendin <nudelkurre@protonmail.com>
2025-04-05 22:32:30 +0200

    Added flag to show current version
    Added flag to get current version from hatch and print it

--- src/ngb/ngb.py
+++ src/ngb/ngb.py
@@ -9,17 +9,30 @@ gi.require_version("Gdk", "4.0")
 
 from gi.repository import Gtk
 from gi.repository import Gdk
+from gi.repository import Gio
+from gi.repository import GLib
 
 import sys
 
 from ngb.widgets import Bluetooth, Clock, Cpu, Disk, Headset, Network, Volume, Weather, Workspaces
 from ngb.modules import Bar, Config
+from ngb import __about__
 
 class MainWindow(Gtk.Application):
     def __init__(self):
-        super().__init__(application_id="com.github.nudelkurre.ngb")
+        super().__init__(application_id="com.github.nudelkurre.ngb",
+                        flags=Gio.ApplicationFlags.FLAGS_NONE |
+                        Gio.ApplicationFlags.HANDLES_COMMAND_LINE)
         self.config = Config()
         self.load_css()
+        self.add_main_option(
+            "version",
+            ord("v"),
+            GLib.OptionFlags.NONE,
+            GLib.OptionArg.NONE,
+            "Show application version",
+            None,
+        )
 
     def do_activate(self):
         for i in self.config.data['bars']:
@@ -104,6 +117,15 @@ class MainWindow(Gtk.Application):
             css_provider,
             Gtk.STYLE_PROVIDER_PRIORITY_USER
         )
+
+    def do_command_line(self, command_line):
+        options = command_line.get_options_dict()
+        options = options.end().unpack()
+        if("version" in options):
+            print(f"Version: {__about__.__version__}")
+        else:
+            self.activate()
+        return True
 
 def main():
     try:
