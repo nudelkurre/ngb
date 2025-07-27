0675b5bd64308cb6df6d2210b9debf8dd3a33947
Emil Wendin <nudelkurre@protonmail.com>
2025-07-28 01:01:29 +0200

    Reimplemented SwayIPC
    Reimplemented SwayIPC to connect to sway socket instead of using a wrapper around i3-ipc library to get id

--- src/ngb/modules/swayipc.py
+++ src/ngb/modules/swayipc.py
@@ -2,20 +2,86 @@ from collections import namedtuple
 import socket
 import re
 import os
-import i3ipc
+import struct
+import json
 
 from .windowmanageripc import WindowManagerIPC
 
-class SwayIPC():
-    conn = i3ipc.Connection()
+class SwayIPC(WindowManagerIPC):
+    sock_req = f"{os.environ['SWAYSOCK']}"
+
+    def send_to_socket(self, cmd):
+        usocket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        try:
+            usocket.connect(self.sock_req)
+            try:
+                usocket.sendall(self.translate_cmd(cmd))
+                usocket.sendall("\n".encode("utf-8"))
+                response = ""
+                while True:
+                    part = usocket.recv(1024)
+                    response += part.decode("utf-8")
+                    if(len(part) < 1024):
+                        break
+                match = re.search(r"\[[\W\w]*\]", response).group(0)
+                if(match):
+                    parsed_response = json.loads(match)
+                else:
+                    parsed_response = []
+                return parsed_response
+            except socket.error as e:
+                print(e)
+        except ConnectionRefusedError:
+            print("Connection to the UNIX socket refused.")
+        except socket.error as e:
+            print(f"Error open socket: {e}")
+        finally:
+            usocket.close()
 
     def get_workspaces(self):
         workspace = namedtuple("workspace", ["id", "name", "focused", "output", "urgent"])
-        wss = self.conn.get_workspaces()
+        wss = self.send_to_socket("GET_WORKSPACES")
         ws_list = list()
         for p in wss:
-            ws_list.append(workspace(id=p.num, name=p.name, focused=p.focused, output=p.output, urgent=p.urgent))
+            ws_list.append(workspace(id=p["num"], name=p["name"], focused=p["focused"], output=p["output"], urgent=p["urgent"]))
         return ws_list
 
-    def command(self, cmd):
-        self.conn.command(cmd)
+    def translate_cmd(self, cmd):
+        match cmd:
+            case "GET_WORKSPACES":
+                cmd_id = 1
+            case "SUBSCRIBE":
+                cmd_id = 2
+            case "GET_OUTPUTS":
+                cmd_id = 3
+            case "GET_TREE":
+                cmd_id = 4
+            case "GET_MARKS":
+                cmd_id = 5
+            case "GET_BAR_CONFIG":
+                cmd_id = 6
+            case "GET_VERSION":
+                cmd_id = 7
+            case "GET_BINDING_MODES":
+                cmd_id = 8
+            case "GET_CONFIG":
+                cmd_id = 9
+            case "SEND_TICK":
+                cmd_id = 10
+            case "SYNC":
+                cmd_id = 11
+            case "GET_BINDING_STATE":
+                cmd_id = 12
+            case "GET_INPUTS":
+                cmd_id = 100
+            case "GET_SEATS":
+                cmd_id = 101
+            case _:
+                cmd_id = 0
+
+        magic_string = "i3-ipc".encode("utf-8")
+        cmd_len = struct.pack("@i", len(cmd))
+        cmd_type = struct.pack("@i", cmd_id)
+
+        cmd_str = magic_string + cmd_len + cmd_type + cmd.encode("utf8")
+        return cmd_str
