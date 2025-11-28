2da63d73a711e23f632241159d4d466ea8646834
Emil Wendin <nudelkurre@protonmail.com>
2025-11-28 03:01:35 +0100

    Fixed battery value not found when headsetcontrol crash
    Fixed battery value not found when headsetcontrol crash which make ngb stuck at last seen value even if headset is turned of until headset is unplugged and replugged in

--- src/ngb/widgets/headset.py
+++ src/ngb/widgets/headset.py
@@ -8,22 +8,28 @@ import re
 
 from ngb.modules import WidgetBox
 
+
 class Headset(WidgetBox):
     def __init__(self, **kwargs):
         self.icon = kwargs.get("icon", "󰋎")
-        self.timer = kwargs.get("timer", 5)
+        self.timer = kwargs.get("timer", 1)
         self.icon_size = kwargs.get("icon_size", 20)
         super().__init__(icon=self.icon, icon_size=self.icon_size, timer=self.timer)
 
     def set_text(self):
         path = which("headsetcontrol")
-        if(path):
-            info = json.loads(subprocess.run("headsetcontrol -o JSON".split(), capture_output=True, text=True).stdout)
+        if path:
+            info = json.loads(
+                subprocess.run(
+                    "headsetcontrol -o JSON".split(), capture_output=True, text=True
+                ).stdout
+            )
             device_battery = []
-            for d in info["devices"]:
-                if(d["battery"]["level"] > 0):
-                    device_battery.append(f"{d['battery']['level']}%")
-            if(len(device_battery) > 0):
+            for d in info.get("devices", []):
+                battery_level = d.get("battery", {}).get("level", 0)
+                if battery_level > 0:
+                    device_battery.append(f"{battery_level}%")
+            if len(device_battery) > 0:
                 self.set_visible(True)
             else:
                 self.set_visible(False)
