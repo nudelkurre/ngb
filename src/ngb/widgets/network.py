3dd3007b6f04634138862233b60c5a65359d0303
Emil Wendin <nudelkurre@protonmail.com>
2025-12-05 16:16:37 +0100

    Changed to only show ip info in dropdown if interface is connected

--- src/ngb/widgets/network.py
+++ src/ngb/widgets/network.py
@@ -6,12 +6,23 @@ import requests
 
 from ngb.modules import WidgetBox
 
+
 class Network(WidgetBox):
     interface = []
-    
+
     def __init__(self, **kwargs):
         self.interface_name = kwargs.get("interface", "")
-        self.interface = psutil.net_if_addrs()[self.interface_name] if self.interface_name in psutil.net_if_addrs() else []
+        self.interface = (
+            psutil.net_if_addrs()[self.interface_name]
+            if self.interface_name in psutil.net_if_addrs()
+            else []
+        )
+        self.is_up = (
+            psutil.net_if_stats()[self.interface_name].isup
+            if self.interface_name in psutil.net_if_stats()
+            else False
+        )
+        self.is_up = False
         self.timer = kwargs.get("timer", 10)
         self.icon = kwargs.get("icon", "󰈀")
         self.icon_size = kwargs.get("icon_size", 20)
@@ -40,39 +51,42 @@ class Network(WidgetBox):
         self.dropdown.add(self.interface_label)
         self.get_mac_address()
         self.dropdown.add(self.mac_address_label)
-        if(self.show_public_ip):
+        if self.show_public_ip:
             self.get_public_ip()
             self.dropdown.add(self.public_ip_header_label)
             self.dropdown.add(self.public_ip_label)
         self.dropdown.add(self.ipv4_header_label)
         self.dropdown.add(self.ipv4_label)
-        if(self.show_ipv6):
+        if self.show_ipv6:
             self.get_ipv6_addrs()
             self.dropdown.add(self.ipv6_header_label)
             self.dropdown.add(self.ipv6_label)
 
     def get_ipv4_addr(self):
-        address = ""
-        for addr in self.interface:
-            if(addr.family == socket.AF_INET and address == ""):
-                address = addr.address
-        if(address != ""):
-            self.text_label.set_label(address)
-            self.ipv4_label.set_label(address)
-            self.ipv4_label.set_visible(True)
-            self.ipv4_header_label.set_visible(True)
+        if self.is_up:
+            address = ""
+            for addr in self.interface:
+                if addr.family == socket.AF_INET and address == "":
+                    address = addr.address
+            if address != "":
+                self.text_label.set_label(address)
+                self.ipv4_label.set_label(address)
+                self.ipv4_label.set_visible(True)
+                self.ipv4_header_label.set_visible(True)
+            else:
+                self.text_label.set_label("N/A")
+                self.ipv4_label.set_visible(False)
+                self.ipv4_header_label.set_visible(False)
         else:
-            self.text_label.set_label("N/A")
-            self.ipv4_label.set_visible(False)
-            self.ipv4_header_label.set_visible(False)
+            self.text_label.set_label("Not connected")
         return True
 
     def get_ipv6_addrs(self):
         addrs = []
         for addr in self.interface:
-            if(addr.family == socket.AF_INET6):
+            if addr.family == socket.AF_INET6:
                 addrs.append(addr.address)
-        if(len(addrs) > 0):
+        if len(addrs) > 0:
             self.ipv6_label.set_label("\n".join(addrs))
             self.ipv6_label.set_visible(True)
             self.ipv6_header_label.set_visible(True)
@@ -89,16 +103,17 @@ class Network(WidgetBox):
     def get_mac_address(self):
         address = ""
         for addr in self.interface:
-            if(addr.family == socket.AF_PACKET):
+            if addr.family == socket.AF_PACKET:
                 address = addr.address
-        if(address != ""):
+        if address != "":
             self.mac_address_label.set_label(f"Mac: {address}")
         else:
             self.mac_address_label.set_label("Mac: N/A")
 
     def on_click(self, user_data):
-        self.populate_dropdown()
-        self.dropdown.popup()
+        if self.is_up:
+            self.populate_dropdown()
+            self.dropdown.popup()
         return True
 
     def on_close(self, user_data):
